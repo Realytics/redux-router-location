ea21835bd795396b9ef2b60314ecfbe950a12f05
Etienne Dldc <etienne.dldc@outlook.fr>
2017-05-31 12:35:03 +0200

    Add tests

--- test/actions.spec.ts
+++ test/actions.spec.ts
@@ -1,4 +1,54 @@
+import { push, replace, go, goBack, goForward, locationDidChange } from '../src/actions';
+import { Location } from 'history';
 
-it('works', () => {
-  expect(2 + 2).toEqual(4);
+function createLocation(path: string = ''): Location {
+  return {
+    pathname: path,
+    hash: '',
+    key: '0uicnx',
+    search: '',
+    state: null,
+  };
+}
+
+describe('actions', () => {
+
+  describe('push', () => {
+    it('create an push action', () => {
+      expect(push('/home')).toMatchSnapshot();
+      expect(push({ pathname: '/home', search: 'test' })).toMatchSnapshot();
+    });
+  });
+
+  describe('replace', () => {
+    it('create an replace action', () => {
+      expect(replace('/home')).toMatchSnapshot();
+      expect(replace({ pathname: '/home', search: 'test' })).toMatchSnapshot();
+    });
+  });
+
+  describe('go', () => {
+    it('create an go action', () => {
+      expect(go(3)).toMatchSnapshot();
+    });
+  });
+
+  describe('goBack', () => {
+    it('create an goBack action', () => {
+      expect(goBack()).toMatchSnapshot();
+    });
+  });
+
+  describe('goForward', () => {
+    it('create an goForward action', () => {
+      expect(goForward()).toMatchSnapshot();
+    });
+  });
+
+  describe('locationDidChange', () => {
+    it('create an locationDidChange action', () => {
+      expect(locationDidChange(createLocation('/home'))).toMatchSnapshot();
+    });
+  });
+
 });
---
+++ test/reducer.spec.ts
@@ -0,0 +1,19 @@
+import { createReducer, RouterState } from '../src/reducer';
+import createBrowserHistory from 'history/createBrowserHistory';
+import { History } from 'history';
+import { isFunction } from 'lodash';
+import { Reducer } from 'redux';
+
+describe('reducer', () => {
+
+  const history: History = createBrowserHistory();
+  
+  it('create a reducer', () => {
+    expect(isFunction(createReducer(history.location))).toEqual(true);
+  });
+  const reducer: Reducer<RouterState> = createReducer(history.location);
+  it('return initiale state if no action', () => {
+    expect(reducer(undefined, { type: 'NOT_REAL_ACTION' })).toEqual({ location: history.location, previousLocation: null });
+  });
+
+});
